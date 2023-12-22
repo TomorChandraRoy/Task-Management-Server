b576707fa65dcdc180b98f00756cfae406391d1d
TomorChandraRoy <tomorchandraroy884@gmail.com>
2023-12-22 17:33:37 +0600

    vercel complete

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.octeyq5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,8 +42,6 @@ async function run() {
       res.send(result);
     });
 
- 
-    
 
    //Tasks  FORM aer single Email data load 
     app.get('/userTasks', async (req, res) => {
